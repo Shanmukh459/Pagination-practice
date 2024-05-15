8ff4199ba6b89298574e23a65740cb5f1a8d7a43
Shanmukh459 <saikrrish459@gmail.com>
2024-05-15 11:07:12 -0700

    working code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = (e) => {
+    const text = e.target.innerText
+    if(text === "First") {
+      setCount(0)
+    } else if(text === "Prev") {
+      setCount(prev => prev-1)
+    } else if(text === "Next") {
+      setCount(prev => prev+1)
+    } else if(text === "Last") {
+      setCount(4)
+    }
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <p>{count}</p>
+      <button disabled={count === 0} onClick={handleClick}>First</button>
+      <button disabled={count === 0} onClick={handleClick}>Prev</button>
+      <button disabled={count === 4} onClick={handleClick}>Next</button>
+      <button disabled={count === 4} onClick={handleClick}>Last</button>
+    </div>
   )
 }
 
